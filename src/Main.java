0b025f065cd13b8ac7013c15debd5c281b419770
DenisKolgatin <dkolgatin@yandex.ru>
2025-04-27 03:31:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double spending  = 250000; //расходы
+        double income    = 276000; //доходы
+        double sum = spending + income; //сумма
+
+        //System.out.printf("Итого (руб): \n %f", sum);//вывод
+        System.out.printf("Итого (руб): \n "+sum); //еще один вывод
+    }
+}
